d31bab7a5ddfab0fcd4226d28377424277eb21c2
CKnuchel <christoph.knuchel@gmail.com>
2024-03-15 19:36:25 +0100

    ADDED: Vanilla Main

---
+++ Shelly/main.c
@@ -0,0 +1,19 @@
+#include <Windows.h>
+#include "helper.c"
+
+/// <summary>
+/// Main entry point for the application
+/// Encrypts Shellscripts
+/// </summary>
+/// <param name="argc">Argument Count</param>
+/// <param name="argv">Arguments</param>
+/// <returns></returns>
+int main(int argc, char* argv[]) {
+
+	if(argc < 2) {
+		puts("Usage: Shelly.exe <shellscript>");
+		return 1;
+	}
+
+
+}
